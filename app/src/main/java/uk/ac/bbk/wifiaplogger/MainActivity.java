1bbbe8944f5a1085a243e7a538f8b38603c25d3e
Andrei Bogomja <bogomja@gmail.com>
2018-12-11 08:32:42 +0000

    Add button for signing user in

--- app/src/main/java/uk/ac/bbk/wifiaplogger/MainActivity.java
+++ app/src/main/java/uk/ac/bbk/wifiaplogger/MainActivity.java
@@ -2,6 +2,8 @@ package uk.ac.bbk.wifiaplogger;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        /* this button is responsible for signing user in with firebase-ui library */
+        final Button signInButton = findViewById(R.id.sign_in_button);
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(final View v) {
+
+            }
+        });
     }
 }
