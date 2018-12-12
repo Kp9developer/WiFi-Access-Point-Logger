80db8046e3a71ac1fda69b70621a65b9984c8698
Andrei Bogomja <bogomja@gmail.com>
2018-12-12 03:06:24 +0000

    Check if user has signed in with FirebaseAuth

--- app/src/main/java/uk/ac/bbk/wifiaplogger/SignedInActivity.java
+++ app/src/main/java/uk/ac/bbk/wifiaplogger/SignedInActivity.java
@@ -3,13 +3,23 @@ package uk.ac.bbk.wifiaplogger;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class SignedInActivity extends AppCompatActivity {
+
+    /* The entry point of the Firebase Authentication SDK */
+    private final FirebaseAuth mFirebaseAuth = FirebaseAuth.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signed_in);
 
+        /* Return to the MainActivity if user hasn't signed in */
+        if (mFirebaseAuth.getCurrentUser() == null) {
+            startActivity(MainActivity.createIntent(this));
+            finish();
+        }
 
     }
 }
