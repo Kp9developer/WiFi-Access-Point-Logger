7f33f3867f34a350fa8900f2d80f34dc0b80a4ed
Andrei Bogomja <bogomja@gmail.com>
2018-12-12 00:12:36 +0000

    Organize imports

--- app/src/main/java/uk/ac/bbk/wifiaplogger/MainActivity.java
+++ app/src/main/java/uk/ac/bbk/wifiaplogger/MainActivity.java
@@ -1,12 +1,32 @@
 package uk.ac.bbk.wifiaplogger;
 
+import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.ErrorCodes;
+import com.firebase.ui.auth.IdpResponse;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.SetOptions;
+import com.google.firebase.iid.FirebaseInstanceId;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static java.util.Objects.requireNonNull;
 
 public class MainActivity extends AppCompatActivity {
 
