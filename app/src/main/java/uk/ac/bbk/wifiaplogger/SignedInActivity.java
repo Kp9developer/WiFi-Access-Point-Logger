bde63a68e826d1467664a933c7a08669c083e9c8
Andrei Bogomja <bogomja@gmail.com>
2018-12-12 03:01:29 +0000

    Add SignedInActivity class

---
+++ app/src/main/java/uk/ac/bbk/wifiaplogger/SignedInActivity.java
@@ -0,0 +1,15 @@
+package uk.ac.bbk.wifiaplogger;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class SignedInActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signed_in);
+
+
+    }
+}
