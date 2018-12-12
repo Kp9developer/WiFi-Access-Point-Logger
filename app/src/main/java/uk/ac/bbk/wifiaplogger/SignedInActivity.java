4cdf888d08231e41b01dbad10e8b41e6b0208397
Andrei Bogomja <bogomja@gmail.com>
2018-12-12 03:15:39 +0000

    Add sign-out functionality

--- app/src/main/java/uk/ac/bbk/wifiaplogger/SignedInActivity.java
+++ app/src/main/java/uk/ac/bbk/wifiaplogger/SignedInActivity.java
@@ -1,11 +1,20 @@
 package uk.ac.bbk.wifiaplogger;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
+import com.firebase.ui.auth.AuthUI;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class SignedInActivity extends AppCompatActivity {
+
+    private static final String TOAST_SIGN_OUT_FAILED = "Sign out failed!";
 
     /* The entry point of the Firebase Authentication SDK */
     private final FirebaseAuth mFirebaseAuth = FirebaseAuth.getInstance();
@@ -21,5 +30,32 @@ public class SignedInActivity extends AppCompatActivity {
             finish();
         }
 
+        /* This button is responsible for signing user out */
+        final Button mSignOutButton = findViewById(R.id.sign_out_button);
+        mSignOutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(final View v) {
+                signOut();
+            }
+        });
+    }
+
+    /**
+     * This method signs users out of {@code SignedInActivity} and returns
+     * them to the {@code MainActivity} if the task is successful.
+     */
+    public void signOut() {
+        AuthUI.getInstance().signOut(SignedInActivity.this)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull final Task<Void> task) {
+                        if (task.isSuccessful()) {
+                            startActivity(MainActivity.createIntent(SignedInActivity.this));
+                            finish();
+                        } else {
+                            Toast.makeText(SignedInActivity.this, TOAST_SIGN_OUT_FAILED, Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
     }
 }
