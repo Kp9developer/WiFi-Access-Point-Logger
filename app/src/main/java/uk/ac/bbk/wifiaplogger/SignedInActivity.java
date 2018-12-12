c6844eecd1b5b367dcc89419abb3c15c2e1f4f0d
Andrei Bogomja <bogomja@gmail.com>
2018-12-12 12:24:24 +0000

    Add method returning intent to create SignedInActivity

--- app/src/main/java/uk/ac/bbk/wifiaplogger/SignedInActivity.java
+++ app/src/main/java/uk/ac/bbk/wifiaplogger/SignedInActivity.java
@@ -1,5 +1,7 @@
 package uk.ac.bbk.wifiaplogger;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
@@ -8,6 +10,8 @@ import android.widget.Button;
 import android.widget.Toast;
 
 import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.IdpResponse;
+import com.firebase.ui.auth.util.ExtraConstants;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
@@ -57,5 +61,20 @@ public class SignedInActivity extends AppCompatActivity {
                         }
                     }
                 });
+    }
+
+    /**
+     * Creates an intent to be used by the {@code MainActivity} class
+     * to launch {@code SignedInActivity}
+     *
+     * @param context     the launching activity
+     * @param idpResponse a container that encapsulates the result of user authentication
+     * @return returns an intent to create {@code SignedInActivity}
+     */
+    public static Intent createIntent(final Context context, final IdpResponse idpResponse) {
+        Intent intent = new Intent();
+        intent.putExtra(ExtraConstants.IDP_RESPONSE, idpResponse);
+        intent.setClass(context, SignedInActivity.class);
+        return intent;
     }
 }
