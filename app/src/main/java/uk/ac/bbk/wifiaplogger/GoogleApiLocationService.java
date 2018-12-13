b9fbf915f49d0606de096668aee1a84120c3dc31
Andrei Bogomja <bogomja@gmail.com>
2018-12-13 22:01:41 +0000

    Fix logging

--- app/src/main/java/uk/ac/bbk/wifiaplogger/GoogleApiLocationService.java
+++ app/src/main/java/uk/ac/bbk/wifiaplogger/GoogleApiLocationService.java
@@ -44,7 +44,7 @@ public class GoogleApiLocationService
         @Override
         public void onLocationResult(final LocationResult locationResult) {
             mLocation = locationResult.getLastLocation();
-            Log.d(TAG, String.format("-25%s loc=%s", "onLocationResult()", mLocation));
+            Log.d(TAG, String.format("%-25s loc=%s", "onLocationResult()", mLocation));
         }
     };
 
@@ -100,7 +100,7 @@ public class GoogleApiLocationService
                         @Override
                         public void onSuccess(final Location location) {
                             mLocation = location;
-                            Log.d(TAG, String.format("-25%s loc=%s", "getLastLocation()", mLocation));
+                            Log.d(TAG, String.format("%-25s loc=%s", "getLastLocation()", mLocation));
                         }
                     });
         }
@@ -122,22 +122,22 @@ public class GoogleApiLocationService
         super.onDestroy();
         mGoogleApiClient.disconnect();
         mFusedLocationProviderClient.removeLocationUpdates(mLocationCallback);
-        Log.d(TAG, String.format("-25%s", "onDestroy()"));
+        Log.d(TAG, String.format("%-25s", "onDestroy()"));
     }
 
     @Override
     public void onConnected(@Nullable final Bundle bundle) {
-        Log.d(TAG, String.format("-25%s bundle=%s", "onConnected(Bundle)", bundle));
+        Log.d(TAG, String.format("%-25s bundle=%s", "onConnected(Bundle)", bundle));
     }
 
     @Override
     public void onConnectionSuspended(final int i) {
-        Log.d(TAG, String.format("-25%s code=%d", "onConnectionSuspended(int)", i));
+        Log.d(TAG, String.format("%-25s code=%d", "onConnectionSuspended(int)", i));
     }
 
     @Override
     public void onConnectionFailed(@NonNull final ConnectionResult connectionResult) {
-        Log.d(TAG, String.format("-25%s msg=%s", "onConnectionFailed(ConnectionResult)", connectionResult.getErrorMessage()));
+        Log.d(TAG, String.format("%-25s msg=%s", "onConnectionFailed(ConnectionResult)", connectionResult.getErrorMessage()));
     }
 
     public class GoogleApiLocationServiceBinder extends Binder {
