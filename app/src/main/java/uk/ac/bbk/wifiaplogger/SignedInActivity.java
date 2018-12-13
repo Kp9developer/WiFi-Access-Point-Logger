c365f634024201daaec06a8a6062b0b3a7afbb30
Andrei Bogomja <bogomja@gmail.com>
2018-12-13 07:29:42 +0000

    Add scan start button

--- app/src/main/java/uk/ac/bbk/wifiaplogger/SignedInActivity.java
+++ app/src/main/java/uk/ac/bbk/wifiaplogger/SignedInActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Spinner;
@@ -62,6 +63,15 @@ public class SignedInActivity extends AppCompatActivity {
                 signOut();
             }
         });
+
+        final Button scanStartButton = findViewById(R.id.scan_start_button);
+        scanStartButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(final View v) {
+
+            }
+        });
+
     }
 
     /**
